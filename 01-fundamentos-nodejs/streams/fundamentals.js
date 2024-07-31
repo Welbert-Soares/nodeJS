9a5be9685a17d58fae2e1aecfdb3002791332547
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-30 22:23:30 -0300

    fundamentos de streams

---
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -0,0 +1,37 @@
+// Streams de Leitura
+
+import { Readable, Transform, Writable } from 'stream';
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(i.toString())
+
+        this.push(buf)
+      }
+    }, 1000) 
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new MultiplyByTenStream())
