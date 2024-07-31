e8c449bb65b68fb8ba3418ef3fc15c5752b08a87
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-30 21:28:12 -0300

    conhecendo http status code

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,18 +1,28 @@
+
 import http from "node:http";
+
+const users = []
 
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === "GET" && url === "/users") {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users))
   }
 
   if (method === "POST" && url === "/users") {
-    return res.end("Criação de usuário")
+    users.push({
+      id: 1,
+      name: "Lucas",
+      email: "lucas@teste.com"
+    })
+
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello World!')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
-
