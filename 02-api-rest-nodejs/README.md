beaf359fee38152d11b5d5fed7174aa1bc77277e
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-08-05 19:00:46 -0300

    resumo de transações

---
+++ 02-api-rest-nodejs/README.md
@@ -0,0 +1,21 @@
+# nodeJS
+- fundamentos - API-REST-NODE
+- Gerenciamento de transações
+
+
+# RF
+
+- [x] O usuáiro de poder criar uma nova transação;
+- [x] O usuário deve poder obter um resumo da sua conta;
+- [x] O usuário deve poder listar todas as transações que já ocorreram;
+- [x] O usuário deve poder visualizar uma transação única;
+
+# RN
+
+- [x] A transação pode ser do tipo crédito que somará ao valor total, ou débito que subtrairá;
+- [] Deve ser possível identificarmos o usuário entre as requisições;
+- [] O usuário só pode visualizar transações o qual ele criou;
+
+# RNF 
+
+- [] 
--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -5,6 +5,38 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transations').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transations').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transations')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return {
+      summary,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
